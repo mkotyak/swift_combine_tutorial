f2989fa6b84b691bc03f792d6ed691d6416cb187
mkotyak <kotyak.maria@gmail.com>
2024-07-29 18:06:55 +0300

    Learned CurrentValueSubject

--- 2. Publishers Starting Stream.playground/Pages/CurrentValueSubject.xcplaygroundpage/Contents.swift
+++ 2. Publishers Starting Stream.playground/Pages/CurrentValueSubject.xcplaygroundpage/Contents.swift
@@ -1,36 +1,37 @@
-//: [Previous](@previous)
+// Subject - Publisher that you can continously send new values down
 
-//Subject - Publisher that you can continously send new values down
+// CurrentValueSubject
+// used like a var with a Publisher stream attached
 
-//CurrentValueSubject
-//used like a var with a Publisher stream attached
-
-import Foundation
 import Combine
-import PlaygroundSupport
-
-PlaygroundPage.current.needsIndefiniteExecution = true
+import Foundation
 
 struct User {
     var id: Int
     var name: String
 }
 
-let currentUserId = CurrentValueSubject<Int     , Never>(1000)
-let userNames     = CurrentValueSubject<[String], Never>(["Bob", "Susan", "Luise"])
+var cancellables = Set<AnyCancellable>()
 
-let currentUser   = CurrentValueSubject<User    , Never>(User(id: 1, name: "Bob"))
+let currentUser = CurrentValueSubject<User, Never>(User(id: 1, name: "Bob"))
+let currentUserId = CurrentValueSubject<Int, Never>(1)
+let userNames = CurrentValueSubject<[String], Never>(["Bob", "Susan", "Luise"])
 
+// Get the value for currentUserID
+print("currentUserId: \(currentUserId.value)")
 
+// Subscribe to currentUserId Subject
+currentUserId
+    .sink {
+        print("Completion: \($0)")
+    } receiveValue: { newValue in
+        print("Recieved value: \(newValue)")
+    }
+    .store(in: &cancellables)
 
-// get the value for currentUserID
+// Passing down new values to currentUserId Subject
+currentUserId.send(2)
+currentUserId.send(3)
 
-// subscribe to Subject
-
-// passing down new values with Subject
-
-// sending completion finished with Subject
-
-
-
-//: [Next](@next)
+// Sending completion finished to currentUserId Subject
+currentUserId.send(completion: .finished)
